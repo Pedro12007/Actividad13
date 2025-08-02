4518838c6b0703ceb0865dd1c6fdb3dcedd38537
Pedro Monroy <monroy.pedroa07@gmail.com>
2025-08-01 23:56:44 -0600

    Estructura inicial; Menú y diccionario de estudiantes

---
+++ main.py
@@ -0,0 +1,39 @@
+students = {}
+
+while True:
+    print('Opciones:')
+    print('1. Agregar estudiantes.')
+    print('2. Agregar curso con nota.')
+    print('3. Consultar estudiante.')
+    print('4. Calcular promedio de estudiante.')
+    print('5. Verificar si un estudiante aprueba.')
+    print('6. Mostrar a todos los estudiantes.')
+    print('7. Salir del programa.')
+
+    option = input('Elija una opción: ')
+
+    match option:
+        case '1':
+            pass
+
+        case '2':
+            pass
+
+        case '3':
+            pass
+
+        case '4':
+            pass
+
+        case '5':
+            pass
+
+        case '6':
+            pass
+
+        case '7':
+            print('Saliendo del programa...')
+            break
+
+        case _:
+            print('Opción inválida. Intente nuevamente.\n')
